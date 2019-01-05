5eaeded66d70bbdadca75c3f414bcbcd20ca8677
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-05 19:40:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,11 +7,9 @@ prime? (integer)
     arr = (1..(integer-1))to_a
     arr.concat((integer+1))
   elsif
-    low_primes = [3,5,7,11]
+    low_primes = [2,3,5,7,11]
     low_primes.include?(integer)
-    
-  elsif integer == 2
-    return true
+
     
     
 end
