1dc15ce7e5fd0a7e153f48cf7538d30f99617129
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-05 20:34:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime? (integer)
   low_primes = [2,3,5,7,11]
-  arr = (1..(integer-1)).to_a
+  arr = (2..(integer-1)).to_a
   
   if integer == 1 || integer == 0
     return false
