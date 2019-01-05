4dcce6e2af34b7a0d387ba10e58eb3056d336ddf
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-05 20:01:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,11 @@
 # Add  code here!
 prime? (integer)
   low_primes = [2,3,5,7,11]
-  
+  arr = (1..(integer-1))to_a
   if integer == 1 || integer == 0
     return false
-  elsif integer > 1 
-    arr = (1..(integer-1))to_a
-    arr.any? do |nums|
-      nums % integer == (1 || 0)
-    end
+  elsif nums % integer == (1 || 0)
+    return true
   elsif integer.even? && integer != 2
     return false
   else
