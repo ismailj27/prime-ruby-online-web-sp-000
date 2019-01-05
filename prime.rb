100ff06b4af39dd81036f4c745ae37a689db135d
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-05 20:23:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,13 @@ def prime? (integer)
   
   if integer == 1 || integer == 0
     return false
+  elsif low_primes.include?(integer)
+    return true
   elsif  % integer == (1 || 0)
     return true
   elsif integer.even? && integer != 2
     return false
   else
-    low_primes.include?(integer)
+    
   end
 end
