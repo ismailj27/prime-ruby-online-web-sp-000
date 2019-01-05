4007d39f920fe4a0f1ceefe84b70ed84694b81fc
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-05 19:53:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,6 +7,7 @@ prime? (integer)
     arr = (1..(integer-1))to_a
     arr.any? do |nums|
       nums % integer == (1 || 0)
+    end
   elsif
     low_primes = [2,3,5,7,11]
     low_primes.include?(integer)
