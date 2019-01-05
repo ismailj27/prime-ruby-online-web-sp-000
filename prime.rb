2d9561549e734d571ad9f32d4e5879ec72b9a971
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-05 19:57:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ prime? (integer)
 
   if integer == 1 || integer == 0
     return false
-  elsif !()
+  elsif !(integer == 1 || integer == 0)
     arr = (1..(integer-1))to_a
     arr.any? do |nums|
       nums % integer == (1 || 0)
